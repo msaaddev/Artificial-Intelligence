dbcd8cdde624876210fb3d43f654ac60e4fd0e48
msaaddev <mrsaadirfan@gmail.com>
2020-10-25 23:04:01 +0500

    👌 IMPROVE: task 8

--- Assignment#1/part03/task8.py
+++ Assignment#1/part03/task8.py
@@ -9,4 +9,22 @@ from random import randint
 
 listOfWords = ["APPLE", "BILBO", "CHORUSED", "DISIMAGINE", "ENSURING", "FORMALISING", "GLITCHES", "HARMINE", "INDENTATION", "JACKED", "KALPACS",
                "LAUNDRY", "MASKED", "NETTED", "OXFORD", "PARODY", "QUOTIENTS", "RACERS", "SADNESS", "THYREOID", "UNDUE", "VENT", "WEDGED", "XERIC", "YOUTHHOOD", "ZIFFS"]
-print("\n Random Word from list:", listOfWords[randint(0, len(listOfWords) - 1)])
+print("\nRandom Word from list:",
+      listOfWords[randint(0, len(listOfWords) - 1)])
+
+# 8.b
+
+randomWord = listOfWords[randint(0, len(listOfWords) - 1)]
+wordGuess = ''.join('_' for i in range(len(randomWord)))
+
+while randomWord != wordGuess:
+    letter = input("Enter a letter : ")
+    letter.lower()
+    if letter in randomWord:
+        indices = [i for i, s in enumerate(randomWord) if s == letter]
+        for i in indices:
+            wordGuess = wordGuess[:i] + letter + wordGuess[i + 1:]
+        print(wordGuess)
+    else:
+        print("Incorrected letter. Try again!")
+print("Congratulations! You won.")
